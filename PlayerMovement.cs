aa2064148412be04273fb1b3e9135510f792e37b
advikaintouch <advikaintouch@gmail.com>
2026-05-29 16:39:36 -0700

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 destination = new Vector3(horizontal, 0, vertical);
+        GetComponent<Rigidbody>().velocity = destination * speed;
+   
+    }
+}
